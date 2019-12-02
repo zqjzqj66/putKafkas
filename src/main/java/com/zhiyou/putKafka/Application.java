5ca894ab2712efb8d376ee42a57a4557481bef3b
zhouqijiang <zhouqijiang@zhiyoutec.com>
2019-12-02 20:48:46 +0800

    BD-000: add code

--- src/main/java/com/zhiyou/putKafka/Application.java
+++ src/main/java/com/zhiyou/putKafka/Application.java
@@ -1,27 +1,30 @@
 package com.zhiyou.putKafka;
 
-import org.apache.tomcat.jni.Thread;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.lang.Thread;
 
 public class Application {
 
-    private static final String topic = "app_get";
-
-    private static final String file="D:\\tmp\\abc.txt";
+    private static final BoneProperty appProperty = new BoneProperty();
+    //获取topic
+    private static final String topic = appProperty.getString("spark.kafka.topics");
+    //获取读取的文件名
+    private static final String fileName = appProperty.getString("file.name");
 
     public static void main(String[] args) {
 
         KafkaService ks = KafkaService.getInstance();
-        int i=0;
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        int i = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = br.readLine()) != null) {
-                if(i%200==0)
-                    java.lang.Thread.sleep(10000);
+                //每发送200条停顿20秒，防止数据量过大，造成kafka堵塞。
+                if (i % 200 == 0) {
+                    Thread.sleep(20000);
+                }
                 ks.send(topic, line);
                 System.out.println(line);
                 i++;
@@ -29,7 +32,14 @@ public class Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    public void test1() {
+        try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
